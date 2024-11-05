89a209cd96ee2879bfdff7433cdf6b5b74c56adf
Dillon-Duncan <mrduncan.business@gmail.com>
2024-11-05 18:38:35 +0200

    Changed starting intent

--- PrixFinanceV3/app/build.gradle.kts
+++ PrixFinanceV3/app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.firebase.auth)
+    implementation("androidx.appcompat:appcompat:1.7.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
